b7e96ceda590ecd01825738bc0f1fbb2bb7de1df
Douche1bag <Ksouythong8@gmail.com>
2025-06-15 00:01:41 +0700

    add pet details

--- petshop/loadpets.js
+++ petshop/loadpets.js
@@ -1,36 +1,37 @@
-var pl = document.getElementById("pet-list");
-
-const pets = [ 
-
-  {"name": "Buddy",
+const pets = [
+  {
+    "name": "Buddy",
     "type": "Dog",
     "age": 3,
-    "img": "petshop-img/dogs/dog01.jpg"}, 
-
-  {"name": "Buddy",
+    "img": "img/dogs/dog01.jpg"
+  },
+  {
+    "name": "muffin",
     "type": "Dog",
     "age": 3,
-    "img": "petshop-img/dogs/dog02.jpg"}, 
+    "img": "img/dogs/dog02.jpg"
+  },
+  { "name": "Vergil", "type": "Dog", "age": 2, "img": "img/dogs/dog03.jpg" },
+  { "name": "Whiskers", "type": "Cat", "age": 2, "img": "img/cats/cat01.jpg" },
+  { "name": "Doomslayer", "type": "Cat", "age": 2, "img": "img/cats/cat02.jpg" },
+  { "name": "Abdula", "type": "Cat", "age": 1, "img": "img/cats/cat03.jpg" },
+  { "name": "THedarkLord", "type": "Bird", "age": 1, "img": "img/birds/bird01.jpg" },
+  { "name": "Voldemort", "type": "Bird", "age": 2, "img": "img/birds/bird02.jpg" },
+  { "name": "Dante", "type": "Capybara", "age": 2, "img": "img/capybaras/capybara01.jpg" },
+  { "name": "Mr.Potter", "type": "Capybara", "age": 3, "img": "img/capybaras/capybara02.jpg" }
 
-  {"name": "Whiskers",
-    "type": "Cat",
-    "age": 2,
-    "img": "petshop-img/cats/cat01.jpg"}, 
+]
 
-  {"name": "Mittens",
-    "type": "Cat",
-    "age": 2,
-    "img": "petshop-img/cats/cat02.jpg"}, 
-
-] 
-
-for(let i = 0; i< pets.length; i++){
-    const pet = pets[i];
-    const petDiv = document.getElementById("pet-list")
-                    .appendChild(document.createElement("div"));
-    petDiv.className = "pet";
-    petDiv.innerHTML = `<img src="${pet.img}" alt="${pet.name}">
-                        <h2>${pet.name}</h2>
-                        <p>Type: ${pet.type}</p>
-                        <p>Age: ${pet.age} years</p>`;
+for (let i = 0; i < pets.length; i++) {
+  const pet = pets[i];
+  const petDiv = document.getElementById('pet-list').appendChild(document.createElement('div'));
+  petDiv.className = 'pet';
+  petDiv.innerHTML = `
+        <img src="${pet.img}" alt="${pet.name}">
+        <h3>${pet.name}</h3>
+        <p>Type: ${pet.type}</p>
+        <p>Age: ${pet.age} years</p>
+        <button onclick="adoptPet()">Adopt Now</button>
+    `;
+  document.getElementById('pet-list').appendChild(petDiv);
 }
